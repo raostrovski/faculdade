49b870dda3d0a1d2effe1671a70c10bb63ad5551
Rafael Ostrovski <alulas.ostrovski@gmail.com>
2018-08-20 19:33:28 -0300

    lançamento do projeto

---
+++ projeto_aula/_js/js.js
@@ -0,0 +1,9 @@
+function menuMobile() {
+    var menu = document.getElementById("menu");
+    if(menu.style.display == 'none'|| menu.style.display==''){
+        menu.style.display = 'flex';
+    }else{
+        menu.style.display='none'
+    }
+
+}
